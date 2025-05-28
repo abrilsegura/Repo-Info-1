f048f3797bde64fabe9ca0783842b21d6773a319
abrilsegura <seguranavarroabril@gmail.com>
2025-05-28 14:48:47 -0300

    new file

--- bmi.c
+++ bmi.c
@@ -13,7 +13,19 @@ int main()
     float bmi= peso/a;
 
     printf("Su índice de masa corporal es: %.2f\n",  bmi);
-    printf(" Índice| Condición\n <18.5 | Bajo peso\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\n >=30| Obesidad\n");
+
+    if(bmi<18.5){
+        printf(" Índice| Condición\n <18.5 | Bajo peso\t#\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\n >=30| Obesidad\n");    
+    }
+    else if(bmi==18.5 || bmi<=24.9){
+        printf(" Índice| Condición\n <18.5 | Bajo peso\n 18.5 a 24.9 | Normal\t#\n 25.0 a 29.9 | Sobrepeso\n >=30| Obesidad\n");
+    }
+    else if(bmi==25 || bmi<=29.9){
+        printf(" Índice| Condición\n <18.5 | Bajo peso\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\t#\n >=30| Obesidad\n");
+    }
+    else {
+        printf(" Índice| Condición\n <18.5 | Bajo peso\n 18.5 a 24.9 | Normal\n 25.0 a 29.9 | Sobrepeso\n >=30| Obesidad\t#\n");
+    }
 
     return 0;
 }
